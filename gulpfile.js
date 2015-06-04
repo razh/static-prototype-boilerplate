09a5d657e12f434122bb041100159af2c2f4d64a
Raymond Zhou <somecrazyidiot@gmail.com>
2015-06-04 16:45:24 -0400

    Add JS task.
    Add support for including CSS in Stylus.

--- gulpfile.js
+++ gulpfile.js
@@ -4,13 +4,20 @@ const SOURCE_DIR = './src';
 const BUILD_DIR = process.env.BUILD_DIR || 'dist';
 const PORT = process.env.PORT || 3000;
 
+const _ = require('lodash');
+const babelify = require('babelify');
+const browserify = require('browserify');
 const browserSync = require('browser-sync').create();
 const del = require('del');
+const glob = require('glob');
+const mergeStream = require('merge-stream');
+const path = require('path');
 const runSequence = require('run-sequence');
+const source = require('vinyl-source-stream');
+const watchify = require('watchify');
 
 const gulp = require('gulp');
-const jade = require('gulp-jade');
-const stylus = require('gulp-stylus');
+const $ = require('gulp-load-plugins')();
 
 function onError(error) {
   console.log(error.message);
@@ -29,7 +36,7 @@ gulp.task('browser-sync', function() {
 
 gulp.task('jade', function() {
   return gulp.src(SOURCE_DIR + '/*.jade')
-     .pipe(jade({doctype: 'html'}))
+     .pipe($.jade({doctype: 'html'}))
      .on('error', onError)
      .pipe(gulp.dest(BUILD_DIR))
      .pipe(browserSync.reload({stream: true}));
@@ -37,22 +44,53 @@ gulp.task('jade', function() {
 
 gulp.task('stylus', function() {
   return gulp.src(SOURCE_DIR + '/*.styl')
-     .pipe(stylus())
+     .pipe($.stylus({
+       'include css': true
+     }))
      .on('error', onError)
      .pipe(gulp.dest(BUILD_DIR))
      .pipe(browserSync.reload({stream: true}));
 });
 
+gulp.task('js', function() {
+  const entries = glob.sync(SOURCE_DIR + '/*.js');
+
+  function bundle(entry) {
+    const bundler = watchify(browserify(entry,
+      _.assign({
+        debug: true
+      }, watchify.args)));
+
+    bundler.transform(babelify);
+
+    function rebundle() {
+      return bundler.bundle()
+        .on('error', onError)
+        .pipe(source(path.basename(entry)))
+        .pipe(gulp.dest(BUILD_DIR))
+        .pipe(browserSync.reload({stream: true}));
+    }
+
+    bundler
+      .on('log', $.util.log)
+      .on('update', rebundle);
+
+    return rebundle();
+  }
+
+  return mergeStream.apply(null, entries.map(bundle));
+});
+
 gulp.task('watch', function() {
   gulp.watch([SOURCE_DIR + '/*.jade'], ['jade']);
   gulp.watch([SOURCE_DIR + '/*.styl'], ['stylus']);
-})
+});
 
 gulp.task('clean', del.bind(null, [BUILD_DIR]));
 
 gulp.task('default', ['clean'], function(cb) {
   return runSequence(
-    ['stylus', 'jade'],
+    ['stylus', 'jade', 'js'],
     ['browser-sync', 'watch'],
     cb
   );
